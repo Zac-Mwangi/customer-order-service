c3ca42079e55ae360ea9670e7be332ecdeb43b42
ragnar <zackmwangi998@gmail.com>
2024-06-27 15:14:23 +0300

    test for authentication

--- CustomerOrderService/settings.py
+++ CustomerOrderService/settings.py
@@ -96,18 +96,18 @@ WSGI_APPLICATION = "CustomerOrderService.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-import dj_database_url
-
 DATABASES = {
-    "default": dj_database_url.parse(env("DATABASE_URL")),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+# import dj_database_url
+
+# DATABASES = {
+#     "default": dj_database_url.parse(env("DATABASE_URL")),
+# }
 
 
 # Password validation
--- customer_app/tests.py
+++ customer_app/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from customer_app.models import Customer
 
-# Create your tests here.
+
+class AuthTests(APITestCase):
+    def setUp(self):
+        self.register_url = reverse("register")
+        self.login_url = reverse("login")
+        self.user_url = reverse("auth-user")
+
+        self.user_data = {
+            "username": "testuser",
+            "email": "testuser@example.com",
+            "password": "testpassword123",
+            "phone_number": "+254712345678",
+            "code": "008",
+        }
+
+    def test_register_user(self):
+        response = self.client.post(self.register_url, self.user_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_login_user(self):
+        # Register a user first
+        response = self.client.post(self.register_url, self.user_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Attempt to log in with registered user credentials
+        login_data = {
+            "email": self.user_data["email"],
+            "password": self.user_data["password"],
+        }
+        response = self.client.post(self.login_url, login_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(
+            "token" in response.data
+        )  # Check if token is present in response
+
+    def test_get_authenticated_user(self):
+        # Register a user first
+        response = self.client.post(self.register_url, self.user_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Log in with registered user credentials
+        login_data = {
+            "email": self.user_data["email"],
+            "password": self.user_data["password"],
+        }
+        response = self.client.post(self.login_url, login_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        token = response.data.get("token", None)
+        self.assertIsNotNone(token)  # Ensure token is retrieved from response
+
+        # Use token for authenticated request
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+        response = self.client.get(self.user_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["user"]["email"], self.user_data["email"])
