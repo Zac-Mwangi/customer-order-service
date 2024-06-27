1d09aba1535bb608025a762f30481c1a9dafd545
ragnar <zackmwangi998@gmail.com>
2024-06-27 13:03:40 +0300

    start of test files

--- CustomerOrderService/settings.py
+++ CustomerOrderService/settings.py
@@ -12,23 +12,33 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import environ
+import os
+
 
 env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = "django-insecure-m6qp0nmcfg77=fey++km!&wzw1haz^b)!+%!)7u(o(b_1t)sar"
-SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
+# DEBUG = env.bool("DEBUG", default=False)
+
 
 ALLOWED_HOSTS = ["*"]
 
